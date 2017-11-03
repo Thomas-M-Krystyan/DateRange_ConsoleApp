5aa6e1fa24241003db417595c1841676a901c3da
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-11-03 10:59:50 +0100

    Initialize application controller and pass arguments to it

---
+++ DateRangeConsoleApplication/Controllers/ApplicationController.cs
@@ -0,0 +1,10 @@
+﻿namespace DateRangeConsoleApplication.Controllers
+{
+    internal class ApplicationController
+    {
+        internal void Start(string[] args)
+        {
+            
+        }
+    }
+}
--- DateRangeConsoleApplication/Program.cs
+++ DateRangeConsoleApplication/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Globalization;
+﻿using DateRangeConsoleApplication.Controllers;
 
 namespace DateRangeConsoleApplication
 {
@@ -7,35 +6,8 @@ namespace DateRangeConsoleApplication
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("The current culture is {0}", CultureInfo.CurrentCulture.DisplayName);
-            CultureInfo.CurrentCulture = new CultureInfo("en-GB");
-            Console.WriteLine("The current culture is {0}", CultureInfo.CurrentCulture.DisplayName);
-            Console.ReadLine();
-
-            DateTime time = DateTime.Now;
-            Console.WriteLine(time.ToString("dd.MM.yyyy"));
-
-            if (args == null)
-            {
-                Console.WriteLine("ERROR: Null!");
-            }
-            else if (args.Length < 2)
-            {
-                Console.WriteLine("ERROR: Less than two elements!");
-            }
-            else if (args.Length > 1 && args.Length < 3)
-            {
-                Console.WriteLine("OK: Two elements");
-                foreach (string element in args)
-                {
-                    Console.WriteLine(element);
-                }
-            }
-            else
-            {
-                Console.WriteLine("ERROR: More than two elements!");
-            }
-            Console.ReadLine();
+            ApplicationController application = new ApplicationController();
+            application.Start(args);
         }
     }
 }
