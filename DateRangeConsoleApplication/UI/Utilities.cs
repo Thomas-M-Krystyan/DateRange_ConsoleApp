4e29c83acf3c0439a1389f67cfa2e0a2566ba77a
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-11-03 16:05:41 +0100

    Add DisplayColor method to error messages and default error message color as constant in GeneralValidation class

---
+++ DateRangeConsoleApplication/UI/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DateRangeConsoleApplication.UI
+{
+    internal static class Utilities
+    {
+        internal static string DisplayColor(string message, string color)
+        {
+            switch (color.ToLower())
+            {
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    break;
+                case "yellow":
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+            }
+            return message;
+        }
+    }
+}
--- DateRangeConsoleApplication/Validation/GeneralValidation.cs
+++ DateRangeConsoleApplication/Validation/GeneralValidation.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using DateRangeConsoleApplication.UI;
 using static DateRangeConsoleApplication.UI.Messages.EnglishMessages;
 
 namespace DateRangeConsoleApplication.Validation
 {
     internal class GeneralValidation<T, TN> where TN : IComparable<TN>
     {
+        private const string ErrorMessageColor = "red";
+
         internal IList<T> ProcessInputData(IList<T> arguments, TN numberOfArguments)
         {
             try
@@ -21,26 +24,32 @@ namespace DateRangeConsoleApplication.Validation
             return null;
         }
 
+        #region Number of arguments
         private static bool ValidNumberOfArguments(IList<T> arguments, TN numberOfArguments)
         {
             if (arguments == null)
             {
-                throw new ArgumentNullException(nameof(arguments), ErrorNullCollection);
+                throw new ArgumentNullException(nameof(arguments),
+                                                Utilities.DisplayColor(message: ErrorNullCollection, color: ErrorMessageColor));
             }
             if (arguments.Count == 0)
             {
-                throw new ArgumentException(ErrorEmptyCollection, nameof(arguments));
+                throw new ArgumentException(Utilities.DisplayColor(message: ErrorEmptyCollection, color: ErrorMessageColor),                                     nameof(arguments));
             }
             if (arguments.Count.CompareTo(numberOfArguments) < 0)
             {
-                throw new ArgumentException(ErrorNotEnoughArguments(numberOfArguments), nameof(arguments));
+                throw new ArgumentException(Utilities.DisplayColor(message: ErrorNotEnoughArguments(numberOfArguments),
+                                                                   color: ErrorMessageColor), nameof(arguments));
             }
             if (arguments.Count.CompareTo(numberOfArguments) > 0)
             {
-                throw new ArgumentOutOfRangeException(nameof(arguments), ErrorToMuchArguments(numberOfArguments));
+                throw new ArgumentOutOfRangeException(nameof(arguments), 
+                                                      Utilities.DisplayColor(message: ErrorToMuchArguments(numberOfArguments),
+                                                                             color: ErrorMessageColor));
             }
 
             return true;
         }
+        #endregion
     }
 }
