9e4f6a56dcbd86b6940c2889eb95e090085b35ea
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-11-04 20:25:54 +0100

    Create new DateRangeController in Controllers folder and declare initial GenerateRange method in it

---
+++ DateRangeConsoleApplication/Controllers/DateRangeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DateRangeConsoleApplication.Controllers
+{
+    internal class DateRangeController
+    {
+        internal string GenerateRange(IList<DateTime> convertedDateCollection)
+        {
+
+
+            return null;
+        }
+    }
+}
