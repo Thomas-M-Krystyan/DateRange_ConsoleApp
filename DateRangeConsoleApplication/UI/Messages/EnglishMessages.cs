3ce9ba7ae2bf25777688e7cd94865b05ff80090e
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-11-04 12:15:16 +0100

    Add new error message to more general error with input non convertible to System.DateTime format, format all error messages by adding word ERROR at the beginning of each one

--- DateRangeConsoleApplication/UI/Messages/EnglishMessages.cs
+++ DateRangeConsoleApplication/UI/Messages/EnglishMessages.cs
@@ -6,20 +6,27 @@ namespace DateRangeConsoleApplication.UI.Messages
     internal static class EnglishMessages
     {
         // Error Messages
-        internal const string ErrorNullCollection = "There is no collection!";
-        internal const string ErrorEmptyCollection = "Collection cannot be empty!";
+        internal const string ErrorNullCollection = "ERROR: There is no collection!";
+        internal const string ErrorEmptyCollection = "ERROR: Collection cannot be empty!";
         
         internal static string ErrorNotEnoughArguments(object numberOfArguments)
         {
-            return $"Collection has less than {numberOfArguments} arguments";
+            return $"ERROR: Collection has less than {numberOfArguments} arguments";
         }
 
         internal static string ErrorToMuchArguments(object numberOfArguments)
         {
-            return $"Collection has more than {numberOfArguments} arguments";
+            return $"ERROR: Collection has more than {numberOfArguments} arguments";
         }
 
-        internal const string ErrorWrongMessageColor = "There is no such message color!";
+        internal const string ErrorWrongMessageColor = "ERROR: There is no such message color!";
+
+        internal static string ErrorInputNotConvertible(object inputValue)
+        {
+            Type dateTimeType = typeof(DateTime);
+
+            return $"ERROR: Input \"{inputValue}\" isn't convertible to {dateTimeType} format";
+        }
 
         internal static string ErrorWrongInputFormat(object inputValue, CultureInfo currentCulture)
         {
@@ -32,7 +39,7 @@ namespace DateRangeConsoleApplication.UI.Messages
             string exampleShortDateFormat = date.ToString("d", currentCulture);
             string exampleLongDateFormat = date.ToString("D", currentCulture);
 
-            return $"Your input \"{inputValue}\" cannot be strictly converted to {dateTimeType} format\n" +
+            return $"ERROR: Your input \"{inputValue}\" cannot be converted to {dateTimeType} format\n" +
                    $"(e.g. you give invalid data type or use wrong date separator, instead of \"{cultureDateSeparator}\")\n" +
                    $"or given date does not exist (e.g. wrong day, month, or year is out of range)!\n\n" +
 
