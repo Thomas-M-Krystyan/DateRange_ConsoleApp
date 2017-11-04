0e91a95196ec458a4cd14732085662f16d6b874a
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-11-04 15:09:19 +0100

    Add method to recognise specific DateTime format (short or long) on given input data

--- DateRangeConsoleApplication/Controllers/ConversionController.cs
+++ DateRangeConsoleApplication/Controllers/ConversionController.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace DateRangeConsoleApplication.Controllers
 {
     internal class ConversionController<T, TN> where T : IComparable
     {
+        // Constants
+        private const string DateTimeIsShort = "short";
+        private const string DateTimeIsLong = "long";
+
         // Controllers
         internal IList<T> ProcessInputData(IList<T> collection)
         {
@@ -14,10 +19,25 @@ namespace DateRangeConsoleApplication.Controllers
         }
 
         // Methods
+        private static string RecogniseDateFormat(T element, CultureInfo currentCulture, out DateTime date)
+        {
+            if (DateTime.TryParseExact(element.ToString(), currentCulture.DateTimeFormat.ShortDatePattern,
+                currentCulture, DateTimeStyles.AssumeLocal, out date))
+            {
+                return DateTimeIsShort;
+            }
+            if (DateTime.TryParseExact(element.ToString(), currentCulture.DateTimeFormat.LongDatePattern,
+                currentCulture, DateTimeStyles.AssumeLocal, out date))
+            {
+                return DateTimeIsLong;
+            }
 
+            return string.Empty;
+        }
+        
         /// <summary>
-        /// Returns specific instance of generic collection, based on input collection type (e.g. array / list)
-        /// </summary>
+         /// Returns specific instance of generic collection, based on input collection type (e.g. array / list)
+         /// </summary>
         private static IList<T> ParseToSpecificCollection(IList<T> collection)
         {
             IList<T> parsedCollection;
