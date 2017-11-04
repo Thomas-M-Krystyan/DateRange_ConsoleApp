ec782559d274218da4a2c403cdb9ebde82548df1
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-11-04 15:01:43 +0100

    Add new method ParseToSpecificCollection() in ConversionController.cs to parse and initialize generic collection by specific instance for further operations on it like adding new elements

--- DateRangeConsoleApplication/Controllers/ConversionController.cs
+++ DateRangeConsoleApplication/Controllers/ConversionController.cs
@@ -5,10 +5,33 @@ namespace DateRangeConsoleApplication.Controllers
 {
     internal class ConversionController<T, TN> where T : IComparable
     {
+        // Controllers
         internal IList<T> ProcessInputData(IList<T> collection)
         {
+            IList<T> convertedDateCollection = ParseToSpecificCollection(collection);
 
             return null;
+        }
+
+        // Methods
+
+        /// <summary>
+        /// Returns specific instance of generic collection, based on input collection type (e.g. array / list)
+        /// </summary>
+        private static IList<T> ParseToSpecificCollection(IList<T> collection)
+        {
+            IList<T> parsedCollection;
+
+            if (collection.IsReadOnly)
+            {
+                parsedCollection = new T[collection.Count];
+            }
+            else
+            {
+                parsedCollection = new List<T>();
+            }
+
+            return parsedCollection;
         }
     }
 }
