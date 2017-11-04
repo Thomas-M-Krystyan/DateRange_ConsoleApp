78ad97ad5dbd017d0504dda2b54855f1ffad55f0
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-11-04 13:53:23 +0100

    Change name of collection parameter from arguments to collection, change where constraint to include generic type "T" as IComparable, instead of "TN"

--- DateRangeConsoleApplication/Program.cs
+++ DateRangeConsoleApplication/Program.cs
@@ -1,4 +1,6 @@
-﻿using DateRangeConsoleApplication.Controllers;
+﻿using System;
+using System.Collections.Generic;
+using DateRangeConsoleApplication.Controllers;
 
 namespace DateRangeConsoleApplication
 {
@@ -11,7 +13,9 @@ namespace DateRangeConsoleApplication
         private static void Main(string[] arguments)
         {
             var application = new ApplicationController<string, int>();
-            application.Start(new string[] {"2017-11-03", "3 listopada 2017"}, NumberOfArguments);
+            application.Start(new List<string>() {"2017-3/03", "3 listopadda 2017"}, NumberOfArguments);
+
+            Console.ReadKey();
         }
     }
 }
